624b71b6ec126b154016eab41b9eba346161da73
Mojashi <akihiro1001@gmail.com>
2021-08-26 20:00:37 +0900

    wip

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/Mojashi/proto-mysql
 
 go 1.15
 
-require github.com/golang/protobuf v1.5.2
+require (
+	github.com/golang/glog v1.0.0
+	github.com/golang/protobuf v1.5.2
+	google.golang.org/protobuf v1.26.0
+)
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
-	"github.com/golang/protobuf/proto"
+	"github.com/golang/glog"
 	descriptor "github.com/golang/protobuf/protoc-gen-go/descriptor"
 	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
+	"google.golang.org/protobuf/internal/errors"
+	"google.golang.org/protobuf/proto"
 )
 
 func parseReq(r io.Reader) (*plugin.CodeGeneratorRequest, error) {
@@ -24,19 +28,103 @@ func parseReq(r io.Reader) (*plugin.CodeGeneratorRequest, error) {
 	return &req, nil
 }
 
+type MySQLDataType string
+
+const MySQLDataTypeMap = map[descriptor.FieldDescriptorProto_Type]MySQLType{
+	descriptor.FieldDescriptorProto_TYPE_DOUBLE:   "DOUBLE",
+	descriptor.FieldDescriptorProto_TYPE_FLOAT:    "FLOAT",
+	descriptor.FieldDescriptorProto_TYPE_INT64:    "BIGINT",
+	descriptor.FieldDescriptorProto_TYPE_UINT64:   "BIGINT UNSIGNED",
+	descriptor.FieldDescriptorProto_TYPE_INT32:    "INT",
+	descriptor.FieldDescriptorProto_TYPE_FIXED64:  "BIGINT UNSIGNED",
+	descriptor.FieldDescriptorProto_TYPE_FIXED32:  "INT",
+	descriptor.FieldDescriptorProto_TYPE_BOOL:     "BOOLEAN",
+	descriptor.FieldDescriptorProto_TYPE_STRING:   "TEXT",
+	descriptor.FieldDescriptorProto_TYPE_BYTES:    "BLOB",
+	descriptor.FieldDescriptorProto_TYPE_UINT32:   "INT UNSIGNED",
+	descriptor.FieldDescriptorProto_TYPE_ENUM:     "TINYINT",
+	descriptor.FieldDescriptorProto_TYPE_SFIXED32: "INT",
+	descriptor.FieldDescriptorProto_TYPE_SFIXED64: "BIGINT",
+	descriptor.FieldDescriptorProto_TYPE_SINT32:   "INT",
+	descriptor.FieldDescriptorProto_TYPE_SINT64:   "BIGINT",
+}
+
+func genMySQLDataType(field *descriptor.FieldDescriptorProto) (MySQLDataType, error) {
+	var mType MySQLDataType
+
+	if field.Type != nil {
+		var ok bool
+		if mType, ok = MySQLDataTypeMap[*field.Type]; !ok {
+			return mType, fmt.Errorf("type %s doesn't have corresponding type in MySQL", field.Type.String())
+		}
+	} else if field.TypeName != nil {
+		return mType, fmt.Errorf("type %s doesn't have corresponding type in MySQL", *field.TypeName)
+	}
+
+	return mType, nil
+}
+
+func genColumnDefinition(field *descriptor.FieldDescriptorProto) (string, error) {
+	dataType, err := genMySQLDataType(field)
+	nullable := "NOT NULL"
+	if field.GetProto3Optional() {
+		nullable = "NULL"
+	}
+	defaultValule := ""
+	if field.DefaultValue != nil {
+		defaultValule = fmt.Sprintf("DEFAULT %s", *field.DefaultValue)
+	}
+
+	return fmt.Sprintf("%s %s %s", dataType, nullable, defaultValule), err
+}
+
+// return column definition. e.g. "id INTEGER NOT NULL"
+func genCreateDefinition(field *descriptor.FieldDescriptorProto) (string, error) {
+	columnDefinition, err := genColumnDefinition(field)
+	if field.GetName() == "" {
+		err = errors.Wrap(err, "field name is empty")
+	}
+	return fmt.Sprintf("%s %s", field.GetName(), columnDefinition), err
+}
+
+func genCreateTable(mt *descriptor.DescriptorProto) string {
+
+	createDefinitions := make([]string, 0, len(mt.Field))
+
+	for _, field := range mt.Field {
+		createDefinition, err := genCreateDefinition(field)
+		if err != nil {
+			glog.Error(err)
+			glog.Errorf("failed to process field %s in message %s", field.GetName(), mt.GetName())
+		}
+		createDefinitions = append(createDefinitions, createDefinition)
+	}
+
+	return fmt.Sprintf("CREATE TABLE %s (%s)",
+		mt.GetName(),
+		strings.Join(createDefinitions, ",\n"),
+	)
+}
+
+func genSQL(f *descriptor.FileDescriptorProto) string {
+	for _, mt := range f.MessageType {
+		genCreateTable(mt)
+	}
+	return ""
+}
+
 func processReq(req *plugin.CodeGeneratorRequest) *plugin.CodeGeneratorResponse {
 	files := make(map[string]*descriptor.FileDescriptorProto)
 	for _, f := range req.ProtoFile {
 		files[f.GetName()] = f
 	}
-
 	var resp plugin.CodeGeneratorResponse
 	for _, fname := range req.FileToGenerate {
 		f := files[fname]
-		out := fname + ".dump"
+		out := fname + ".sql"
 		resp.File = append(resp.File, &plugin.CodeGeneratorResponse_File{
 			Name:    proto.String(out),
-			Content: proto.String(proto.MarshalTextString(f)),
+			Content: proto.String(genSQL(f)),
 		})
 	}
 	return &resp
