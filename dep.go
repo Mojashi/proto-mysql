a796717020cb149d2d904c427dd9f5779477fa9e
Mojashi <akihiro1001@gmail.com>
2021-08-27 22:25:16 +0900

    seiri

--- dep.go
+++ dep.go
@@ -12,12 +12,12 @@ import (
 type Path []string
 
 type INameSpace interface {
-	getMessage(path Path) (Message, bool)
-	getEnum(path Path) (Enum, bool)
-	getNameSpace(name Path) *NameSpace
-	addEnum(enum Enum)
-	addMessage(Message Message)
-	printTree(depth int) string
+	GetMessage(path Path) (Message, bool)
+	GetEnum(path Path) (Enum, bool)
+	GetNameSpace(name Path) *NameSpace
+	AddEnum(enum Enum)
+	AddMessage(Message Message)
+	PrintTree(depth int) string
 }
 
 //impl NameSpace
@@ -35,49 +35,49 @@ func NewNameSpace() *NameSpace {
 	}
 }
 
-func (ns *NameSpace) getMessage(path Path) (Message, bool) {
+func (ns *NameSpace) GetMessage(path Path) (Message, bool) {
 	if len(path) == 0 {
 		return Message{}, false
 	}
 	if path[0] == "" {
-		return ns.getMessage(path[1:])
+		return ns.GetMessage(path[1:])
 	}
 
 	if len(path) == 1 {
 		msg, ok := ns.messages[path[0]]
 		return msg, ok
 	} else if next, ok := ns.childNameSpaces[path[0]]; ok {
-		return next.getMessage(path[1:])
+		return next.GetMessage(path[1:])
 	} else {
 		return Message{}, false
 	}
 }
 
-func (ns *NameSpace) getEnum(path Path) (Enum, bool) {
+func (ns *NameSpace) GetEnum(path Path) (Enum, bool) {
 	if len(path) == 0 {
 		return Enum{}, false
 	}
 	if path[0] == "" {
-		return ns.getEnum(path[1:])
+		return ns.GetEnum(path[1:])
 	}
 
 	if len(path) == 1 {
 		enum, ok := ns.enums[path[0]]
 		return enum, ok
 	} else if next, ok := ns.childNameSpaces[path[0]]; ok {
-		return next.getEnum(path[1:])
+		return next.GetEnum(path[1:])
 	} else {
 		return Enum{}, false
 	}
 }
 
 //package "foo.bar" -> getNameSpace([]string{"foo", "bar"})
-func (ns *NameSpace) getNameSpace(name Path) *NameSpace {
+func (ns *NameSpace) GetNameSpace(name Path) *NameSpace {
 	if len(name) == 0 {
 		return ns
 	}
 	if name[0] == "" {
-		return ns.getNameSpace(name[1:])
+		return ns.GetNameSpace(name[1:])
 	}
 	var ch INameSpace
 	var ok bool
@@ -85,21 +85,21 @@ func (ns *NameSpace) getNameSpace(name Path) *NameSpace {
 		ch = NewNameSpace()
 		ns.childNameSpaces[name[0]] = ch
 	}
-	return ch.getNameSpace(name[1:])
+	return ch.GetNameSpace(name[1:])
 }
 
-func (ns *NameSpace) addMessage(message Message) {
+func (ns *NameSpace) AddMessage(message Message) {
 	ns.messages[message.message.GetName()] = message
 	ns.childNameSpaces[message.message.GetName()] = &message
 }
-func (ns *NameSpace) addEnum(enum Enum) {
+func (ns *NameSpace) AddEnum(enum Enum) {
 	ns.enums[enum.enum.GetName()] = enum
 }
 
-func (ns *NameSpace) printTree(depth int) string {
+func (ns *NameSpace) PrintTree(depth int) string {
 	trees := []string{}
 	for name, dep := range ns.childNameSpaces {
-		trees = append(trees, strings.Repeat("\t", depth)+name+":"+dep.printTree(depth+1))
+		trees = append(trees, strings.Repeat("\t", depth)+name+":"+dep.PrintTree(depth+1))
 	}
 	messages := []string{}
 	for name, _ := range ns.messages {
@@ -129,10 +129,10 @@ func NewMessage(message *descriptor.DescriptorProto) Message {
 		message,
 	}
 	for _, enum := range message.GetEnumType() {
-		ret.addEnum(NewEnum(enum))
+		ret.AddEnum(NewEnum(enum))
 	}
 	for _, message := range message.GetNestedType() {
-		ret.addMessage(NewMessage(message))
+		ret.AddMessage(NewMessage(message))
 	}
 	return ret
 }
@@ -156,12 +156,12 @@ func analyzeDependency(req *plugin.CodeGeneratorRequest, file *descriptor.FileDe
 	}
 
 	analyzeFile := func(f *descriptor.FileDescriptorProto) {
-		cns := ns.getNameSpace(strings.Split(f.GetPackage(), "."))
+		cns := ns.GetNameSpace(strings.Split(f.GetPackage(), "."))
 		for _, message := range f.GetMessageType() {
-			cns.addMessage(NewMessage(message))
+			cns.AddMessage(NewMessage(message))
 		}
 		for _, enum := range f.GetEnumType() {
-			cns.addEnum(NewEnum(enum))
+			cns.AddEnum(NewEnum(enum))
 		}
 	}
 
@@ -173,6 +173,6 @@ func analyzeDependency(req *plugin.CodeGeneratorRequest, file *descriptor.FileDe
 		}
 	}
 	analyzeFile(file)
-	glog.Info(ns.printTree(0))
+	glog.Info(ns.PrintTree(0))
 	return ns
 }
--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"strings"
 
-	"github.com/golang/glog"
 	descriptor "github.com/golang/protobuf/protoc-gen-go/descriptor"
 	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
-	"github.com/pkg/errors"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -26,113 +22,6 @@ func parseReq(r io.Reader) (*plugin.CodeGeneratorRequest, error) {
 		return nil, err
 	}
 	return &req, nil
-}
-
-type MySQLDataType string
-
-var MySQLDataTypeMap = map[descriptor.FieldDescriptorProto_Type]MySQLDataType{
-	descriptor.FieldDescriptorProto_TYPE_DOUBLE:   "DOUBLE",
-	descriptor.FieldDescriptorProto_TYPE_FLOAT:    "FLOAT",
-	descriptor.FieldDescriptorProto_TYPE_INT64:    "BIGINT",
-	descriptor.FieldDescriptorProto_TYPE_UINT64:   "BIGINT UNSIGNED",
-	descriptor.FieldDescriptorProto_TYPE_INT32:    "INT",
-	descriptor.FieldDescriptorProto_TYPE_FIXED64:  "BIGINT UNSIGNED",
-	descriptor.FieldDescriptorProto_TYPE_FIXED32:  "INT",
-	descriptor.FieldDescriptorProto_TYPE_BOOL:     "BOOLEAN",
-	descriptor.FieldDescriptorProto_TYPE_STRING:   "TEXT",
-	descriptor.FieldDescriptorProto_TYPE_BYTES:    "BLOB",
-	descriptor.FieldDescriptorProto_TYPE_UINT32:   "INT UNSIGNED",
-	descriptor.FieldDescriptorProto_TYPE_ENUM:     "ENUM",
-	descriptor.FieldDescriptorProto_TYPE_SFIXED32: "INT",
-	descriptor.FieldDescriptorProto_TYPE_SFIXED64: "BIGINT",
-	descriptor.FieldDescriptorProto_TYPE_SINT32:   "INT",
-	descriptor.FieldDescriptorProto_TYPE_SINT64:   "BIGINT",
-}
-
-func enumEnum(e *descriptor.EnumDescriptorProto) (names []string) {
-	vs := e.GetValue()
-	names = make([]string, 0, len(vs))
-	for i := 0; len(vs) > i; i++ {
-		names = append(names, string(vs[i].GetName()))
-	}
-	return names
-}
-
-func genMySQLDataType(dep INameSpace, field *descriptor.FieldDescriptorProto) (MySQLDataType, error) {
-	var mType MySQLDataType
-
-	if field.Type != nil {
-		var ok bool
-		if mType, ok = MySQLDataTypeMap[field.GetType()]; !ok {
-			return mType, fmt.Errorf("type %s doesn't have corresponding type in MySQL", field.Type.String())
-		}
-		if mType == "ENUM" {
-			if enum, ok := dep.getEnum(strings.Split(field.GetTypeName(), ".")); ok {
-				mType += MySQLDataType("(\"" + strings.Join(enumEnum(enum.enum), "\",\"") + "\")")
-			} else {
-				glog.Errorf("failed to find ENUM %s", field.GetTypeName())
-				return mType, fmt.Errorf("failed to find ENUM")
-			}
-		}
-	} else if field.TypeName != nil {
-		return mType, fmt.Errorf("type %s doesn't have corresponding type in MySQL", *field.TypeName)
-	} else {
-		return mType, fmt.Errorf("failed to find type")
-	}
-
-	return mType, nil
-}
-
-func genColumnDefinition(dep INameSpace, field *descriptor.FieldDescriptorProto) (string, error) {
-	dataType, err := genMySQLDataType(dep, field)
-	nullable := "NOT NULL"
-	if field.GetProto3Optional() {
-		nullable = "NULL"
-	}
-	defaultValule := ""
-	if field.DefaultValue != nil {
-		defaultValule = fmt.Sprintf("DEFAULT %s", *field.DefaultValue)
-	}
-
-	return fmt.Sprintf("%s %s %s", dataType, nullable, defaultValule), err
-}
-
-// return column definition. e.g. "id INTEGER NOT NULL"
-func genCreateDefinition(dep INameSpace, field *descriptor.FieldDescriptorProto) (string, error) {
-	columnDefinition, err := genColumnDefinition(dep, field)
-	if field.GetName() == "" {
-		err = errors.Wrap(err, "field name is empty")
-	}
-	return fmt.Sprintf("%s %s", field.GetName(), columnDefinition), err
-}
-
-func genCreateTable(dep INameSpace, mt *descriptor.DescriptorProto) string {
-
-	createDefinitions := make([]string, 0, len(mt.Field))
-
-	for _, field := range mt.Field {
-		createDefinition, err := genCreateDefinition(dep, field)
-		if err != nil {
-			glog.Error(err)
-			glog.Errorf("failed to process field %s in message %s", field.GetName(), mt.GetName())
-		}
-		createDefinitions = append(createDefinitions, "\t"+createDefinition)
-	}
-
-	return fmt.Sprintf("CREATE TABLE %s (\n%s\n);",
-		mt.GetName(),
-		strings.Join(createDefinitions, ",\n"),
-	)
-}
-
-func genSQL(dep INameSpace, f *descriptor.FileDescriptorProto) string {
-	log.Print(f.Dependency)
-	log.Print(f.EnumType)
-	createTables := make([]string, 0, len(f.MessageType))
-	for _, mt := range f.MessageType {
-		createTables = append(createTables, genCreateTable(dep, mt))
-	}
-	return strings.Join(createTables, "\n\n")
 }
 
 func processReq(req *plugin.CodeGeneratorRequest) *plugin.CodeGeneratorResponse {
